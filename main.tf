d3af966df11eb92c74a3e575f8927f65bdab8a88
Kien Dolorfo <37242789+kee-en@users.noreply.github.com>
2022-07-20 17:32:44 +0800

    Update 07202022

---
+++ main.tf
@@ -0,0 +1,160 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+# Create VPC
+resource "aws_vpc" "kien-vpc" {
+  cidr_block       = var.cidr.vpc_block
+  instance_tenancy = "default"
+
+  tags = {
+    Name        = var.tag_list.vpc_name
+    GBL_CLASS_0 = var.tag_list.GBL_CLASS_0
+    GBL_CLASS_1 = var.tag_list.GBL_CLASS_1
+  }
+}
+
+#Create Subnet Public 1
+resource "aws_subnet" "kien-pub-sub-1" {
+  vpc_id     = aws_vpc.kien-vpc.id
+  cidr_block = var.cidr.sub_pub1_block
+
+  tags = {
+    Name        = var.tag_list.sub_pub1_name
+    GBL_CLASS_0 = var.tag_list.GBL_CLASS_0
+    GBL_CLASS_1 = var.tag_list.GBL_CLASS_1
+  }
+}
+
+#Create Subnet Public 2
+resource "aws_subnet" "kien-pub-sub-2" {
+  vpc_id     = aws_vpc.kien-vpc.id
+  cidr_block = var.cidr.sub_pub2_block
+
+  tags = {
+    Name        = var.tag_list.sub_pub2_name
+    GBL_CLASS_0 = var.tag_list.GBL_CLASS_0
+    GBL_CLASS_1 = var.tag_list.GBL_CLASS_1
+  }
+}
+
+#Create Subnet Private 1
+resource "aws_subnet" "kien-pri-sub-1" {
+  vpc_id     = aws_vpc.kien-vpc.id
+  cidr_block = var.cidr.sub_pri1_block
+
+  tags = {
+    Name        = var.tag_list.sub_pri1_name
+    GBL_CLASS_0 = var.tag_list.GBL_CLASS_0
+    GBL_CLASS_1 = var.tag_list.GBL_CLASS_1
+  }
+}
+
+#Create Subnet Private 2
+resource "aws_subnet" "kien-pri-sub-2" {
+  vpc_id     = aws_vpc.kien-vpc.id
+  cidr_block = var.cidr.sub_pri2_block
+
+  tags = {
+    Name        = var.tag_list.sub_pri2_name
+    GBL_CLASS_0 = var.tag_list.GBL_CLASS_0
+    GBL_CLASS_1 = var.tag_list.GBL_CLASS_1
+  }
+}
+
+#Create Subnet DB 1
+resource "aws_subnet" "kien-db-sub-1" {
+  vpc_id     = aws_vpc.kien-vpc.id
+  cidr_block = var.cidr.sub_db1_block
+
+  tags = {
+    Name        = var.tag_list.sub_db1_name
+    GBL_CLASS_0 = var.tag_list.GBL_CLASS_0
+    GBL_CLASS_1 = var.tag_list.GBL_CLASS_1
+  }
+}
+
+#Create Subnet DB 2
+resource "aws_subnet" "kien-db-sub-2" {
+  vpc_id     = aws_vpc.kien-vpc.id
+  cidr_block = var.cidr.sub_db2_block
+
+  tags = {
+    Name        = var.tag_list.sub_db2_name
+    GBL_CLASS_0 = var.tag_list.GBL_CLASS_0
+    GBL_CLASS_1 = var.tag_list.GBL_CLASS_1
+  }
+}
+
+#Create Internet Gateway
+resource "aws_internet_gateway" "kien-igw" {
+  vpc_id = aws_vpc.kien-vpc.id
+
+  tags = {
+    Name        = var.tag_list.igw_name
+    GBL_CLASS_0 = var.tag_list.GBL_CLASS_0
+    GBL_CLASS_1 = var.tag_list.GBL_CLASS_1
+  }
+}
+
+#Create Nat Gateway in Public Subnet
+resource "aws_nat_gateway" "kien-ngw" {
+  subnet_id = aws_subnet.kien-pub-sub-1.id
+
+  tags = {
+    Name        = var.tag_list.ngw_name
+    GBL_CLASS_0 = var.tag_list.GBL_CLASS_0
+    GBL_CLASS_1 = var.tag_list.GBL_CLASS_1
+  }
+}
+
+#Create RouteTable Public with Internet Gateway
+resource "aws_route_table" "kien-rt-public" {
+  vpc_id = aws_vpc.kien-vpc.id
+
+  route {
+    cidr_block = var.cidr.all_block
+    gateway_id = aws_internet_gateway.kien-igw.id
+  }
+
+  tags = {
+    Name        = var.tag_list.rt_pub_name
+    GBL_CLASS_0 = var.tag_list.GBL_CLASS_0
+    GBL_CLASS_1 = var.tag_list.GBL_CLASS_1
+  }
+}
+
+resource "aws_route_table_association" "kien-rta-pub-1" {
+  subnet_id      = aws_subnet.kien-pub-sub-1.id
+  route_table_id = aws_route_table.kien-rt-public.id
+}
+
+resource "aws_route_table_association" "kien-rta-pub-2" {
+  subnet_id      = aws_subnet.kien-pub-sub-2.id
+  route_table_id = aws_route_table.kien-rt-public.id
+}
+
+#Create RouteTable Private with Nat Gateway
+resource "aws_route_table" "kien-rt-private" {
+  vpc_id = aws_vpc.kien-vpc.id
+
+  route {
+    cidr_block = var.cidr.all_block
+    gateway_id = aws_nat_gateway.kien-ngw.id
+  }
+
+  tags = {
+    Name        = var.tag_list.rt_pri_name
+    GBL_CLASS_0 = var.tag_list.GBL_CLASS_0
+    GBL_CLASS_1 = var.tag_list.GBL_CLASS_1
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,57 @@
+variable "tag_list" {
+  type = object({
+    GBL_CLASS_0   = string
+    GBL_CLASS_1   = string
+    vpc_name      = string
+    sub_pub1_name = string
+    sub_pub2_name = string
+    sub_pri1_name = string
+    sub_pri2_name = string
+    sub_db1_name  = string
+    sub_db2_name  = string
+    igw_name      = string
+    ngw_name      = string
+    rt_pub_name   = string
+    rt_pri_name   = string
+  })
+
+  default = {
+    GBL_CLASS_0   = "SERVICE"
+    GBL_CLASS_1   = "TEST"
+    vpc_name      = "kien-vpc"
+    sub_pub1_name = "kien-pub-sub-1"
+    sub_pub2_name = "kien-pub-sub-2"
+    sub_pri1_name = "kien-pri-sub-1"
+    sub_pri2_name = "kien-pri-sub-2"
+    sub_db1_name  = "kien-db-sub-1"
+    sub_db2_name  = "kien-db-sub-2"
+    igw_name      = "kien-igw"
+    ngw_name      = "kien-ngw"
+    rt_pub_name   = "kien-rt-public"
+    rt_pri_name   = "kien-rt-private"
+  }
+}
+
+variable "cidr" {
+  type = object({
+    vpc_block      = string
+    sub_pub1_block = string
+    sub_pub2_block = string
+    sub_pri1_block = string
+    sub_pri2_block = string
+    sub_db1_block  = string
+    sub_db2_block  = string
+    all_block      = string
+  })
+
+  default = {
+    all_block      = "0.0.0.0/0"
+    sub_db1_block  = "10.50.7.0/24"
+    sub_db2_block  = "10.50.8.0/24"
+    sub_pri1_block = "10.50.4.0/24"
+    sub_pri2_block = "10.50.5.0/24"
+    sub_pub1_block = "10.50.1.0/24"
+    sub_pub2_block = "10.50.2.0/24"
+    vpc_block      = "10.50.0.0/16"
+  }
+}
