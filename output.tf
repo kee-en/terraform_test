1150342a030fd3efa5e8cb3c9b397b377a69d9e1
Kien Dolorfo <37242789+kee-en@users.noreply.github.com>
2022-07-25 17:12:07 +0800

    Update Subnet modules

--- output.tf
+++ output.tf
@@ -5,3 +5,23 @@ output "vpc_id" {
 output "pub_sub_1_id" {
   value = module.subnets.public_subnet_1_id
 }
+
+output "pub_sub_2_id" {
+  value = module.subnets.public_subnet_2_id
+}
+
+output "pri_sub_1_id" {
+  value = module.subnets.private_subnet_1_id
+}
+
+output "pri_sub_2_id" {
+  value = module.subnets.private_subnet_2_id
+}
+
+output "db_sub_1_id" {
+  value = module.subnets.db_subnet_1_id
+}
+
+output "db_sub_2_id" {
+  value = module.subnets.db_subnet_2_id
+}
